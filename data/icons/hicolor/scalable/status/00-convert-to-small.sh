3066549663110c0aa02190a1b7255eef51ab9aa1
Vitaly Dyachkov <obyknovenius@me.com>
2020-11-18 17:17:54 +0100

    Replace small icons with symbolic icons from Adwaita and color them

--- data/icons/hicolor/scalable/status/00-convert-to-small.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-svgs="weather-clear weather-clear-night weather-few-clouds weather-few-clouds-night weather-fog weather-overcast weather-severe-alert weather-showers weather-showers-scattered weather-snow weather-storm weather-tornado weather-windy"
-for svg in $svgs
-do
-    rsvg-convert -a -w 64 -f svg $svg.svg -o $svg-small.svg
-done
